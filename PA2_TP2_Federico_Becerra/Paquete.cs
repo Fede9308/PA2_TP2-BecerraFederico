1a21d944b95c65092e4b2637bb8bcb9f0ccab15e
Fede9308 <117412714+Fede9308@users.noreply.github.com>
2023-05-13 21:17:33 -0300

    first commit

---
+++ PA2_TP2_Federico_Becerra/Paquete.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PA2_TP2_Federico_Becerra
+{
+    public class Paquete
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public int TotalDias { get; set; }
+        public int TotalNoches { get; set; }
+        public string CondicionComercial { get; set; }
+
+        public IList<Ciudad> CiudadList { get; }
+
+        public Paquete()
+        {
+            CiudadList = new List<Ciudad>();
+        }
+        public Paquete(int codigo, string nombre, int totalDias, int totalNoches, string condicionComercial)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            TotalDias = totalDias;
+            TotalNoches = totalNoches;
+            CondicionComercial = condicionComercial;
+            CiudadList = new List<Ciudad>();
+        }
+
+        public void AgregarCiudad(Ciudad ciudad)
+        {
+            if (ciudad != null && !CiudadList.Contains(ciudad))
+            {
+                CiudadList.Add(ciudad);
+            }
+        }
+    }
+}
---
+++ PA2_TP2_Federico_Becerra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PA2_TP2_Federico_Becerra 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Paquete paquete1 = new Paquete();
+        }
+    }
+}
